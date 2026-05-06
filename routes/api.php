54b3e15b32bef308466c96cf6844e92cd822a46e
programerssdk <rehanbhaifromsdk@gmail.com>
2026-05-06 17:52:56 +0500

    api systing

--- routes/api.php
+++ routes/api.php
@@ -7,20 +7,12 @@ use App\Http\Controllers\API\StudentController;
 use App\Http\Controllers\API\FeeInvoiceController;
 use App\Http\Controllers\ReceiptController;
 
-// Auth Routes
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    // Auth User
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
-
-    // Students API
     Route::apiResource('students', StudentController::class);
-
-    // Invoices API
     Route::apiResource('invoices', FeeInvoiceController::class);
-
-    // PDF Generation API
     Route::get('/invoices/{invoice}/pdf', [ReceiptController::class, 'generateReceipt']);
 });
